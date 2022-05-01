f228c05fe1009a973e245f0d362c80eaa2a6d8d7
X4NDER78 <100023467+X4NDER78@users.noreply.github.com>
2022-05-01 19:26:58 +0300

    Add files via upload

--- HW_Hillel/hw7.py
+++ HW_Hillel/hw7.py
@@ -1,14 +1,14 @@
 # HW7
 
 import random
-def get_number(age):
-    """
+def get_number():
+    while True:
+        try:
+            return int(input('Введіть свій вік -> '))
+        except Exception as e:
+            print('Це не може бути ваш вік')
 
-    :return:
-    """
-
-    age = input()
-    return
+age = get_number()
 def digits(age):
     """
     FUNCTION FIND NUMBERS WITH THE SAME NUMERIC
@@ -48,10 +48,10 @@ def form_of_age(age):
 
     if age % 10 == 1 and age != 11:
         res = 'рік'
-    elif age % 10 == 2 or age % 10 == 3 or age % 10 == 4:
-        res = 'роки'
     elif age % 10 == 5 or age % 10 == 6 or age % 10 == 7 or age % 10 == 8 or age % 10 == 9 or age == exc or age % 10 == 0:
         res = 'років'
+    elif age % 10 == 2 or age % 10 == 3 or age % 10 == 4:
+        res = 'роки'
     else:
         print('На жаль , я не можу вам допомгти')
     return res
@@ -67,16 +67,16 @@ def age_control(age):
 
     Returns:CORRECT ANSWER THAT DEPENDS ON THE AGE
     """
-    if age < 7:
-        print(f'Тобі ж {age} {year}! Де твої батьки?')
-    elif age < 16:
-        print(f'Тобі лише {age} {year}, а це е фільм для дорослих!')
-    elif age > 65:
-        print(f'Вам {age} {year}? Покажіть пенсійне посвідчення!')
-    elif len(set(repdigit)) != len(repdigit):
+    if len(set(repdigit)) != len(repdigit):
         print(f'О вам {age} {year}! Який цікавий вік')
-    else:
-        print(f'Незважаючи на те, що вам {age} {year}, білетів всеодно нема!')
+    if age == 0:
+        print('Ти маєшь бути в іншему місці')
+    elif age < 7 and age != 0:
+        print(f'Тобі ж {age} {year}! Де твої батьки?')
+    elif age < 16 and age != 0 and age != 11:
+        print(f'Тобі лише {age} {year}, а це е фільм для дорослих!')
+    elif age > 65 and age != 0 and age < 130 and age != 66 and age != 77 and age != 88 and age != 99 and age != 111:
+        print(f'Вам {age} {year}? Покажіть пенсійне посвідчення!')
 
 result = age_control(age)
 
